ca4e33a1714c69293832cd0d877c37cd3e65999d
Francesco Minna <fra1995m@gmail.com>
2024-03-14 09:27:41 +0100

    Update kubescape_fix_chart.py

--- .github/scripts/kubescape_fix_chart.py
+++ .github/scripts/kubescape_fix_chart.py
@@ -289,6 +289,7 @@ class LookupClass:
         "C-0042": "check_68",
         "C-0237": "",
         "C-0211": "",
+        "C-0210": "",
         "C-0035": "",
         "C-0026": "", # Kubernetes CronJob
         "C-0012": "", # Applications credentials in configuration file
